0b14157150d9774135c6efa33b720219f491345d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-08-27 20:21:00 +0500

    created DirectionsSection component

---
+++ components/DirectionsSection.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { directionsData } from "@/lib/data";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Code2, Languages, Cpu, Palette, Trophy, ArrowRight, Sparkles } from "lucide-react";
+
+
+export default function DirectionsSection() {
+  return (
+  );
+}
