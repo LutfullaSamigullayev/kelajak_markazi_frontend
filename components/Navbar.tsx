d339a3b6363c448b0d3182a3adee1ce2d6f0db28
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-09-01 19:02:00 +0500

    Navbar added mobileMenuOpen state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,6 +21,7 @@ import {
 export default function Navbar() {
   const pathname = usePathname();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="sticky top-0 z-50 w-full transition-all duration-300">
