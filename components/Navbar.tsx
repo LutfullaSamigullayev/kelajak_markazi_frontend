1fd6f006fdb19226dab1bfa2971e5db37c1a4eb3
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-09-02 19:23:00 +0500

    Navbar added navLinks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -230,6 +230,16 @@ export default function Navbar() {
       {/* Mobile Drawer Menu */}
       {mobileMenuOpen && (
         <div className="lg:hidden fixed inset-x-0 top-[102px] bg-white dark:bg-[#120924] border-b border-[#E8DFED] dark:border-purple-900 shadow-2xl p-4 z-40 max-h-[85vh] overflow-y-auto animate-in slide-in-from-top duration-300">
+          <div className="flex flex-col gap-2">
+            {navLinks.map((link) => {
+              if (link.dropdown) {
+                return (
+                );
+              }
+              return (
+              );
+            })}
+          </div>
         </div>
       )}
     </header>
