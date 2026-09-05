12888d906e5e02596bf861e29947deb28a272f49
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-09-05 19:57:00 +0500

    created StudentCouncilPage

---
+++ app/student-council/page.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Sparkles, Trophy, Users, Star, Award, ShieldAlert, HeartHandshake } from "lucide-react";
+
+
+export default function StudentCouncilPage() {
+  return (
+    <div className="py-16 bg-[#F9F6FC] dark:bg-[#120924] min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-12">
+      </div>
+    </div>
+  );
+}
