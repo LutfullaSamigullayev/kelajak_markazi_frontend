7b6b169b868c9a70942ccd29aedb59b5ab4ef63b
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-09-05 19:53:00 +0500

    NewsSection added Clock

--- components/NewsSection.tsx
+++ components/NewsSection.tsx
@@ -55,6 +55,10 @@ export default function NewsSection() {
                         <Calendar className="w-3.5 h-3.5 text-[#5C2D91]" />
                         {article.date}
                       </span>
+                      <span className="flex items-center gap-1">
+                        <Clock className="w-3.5 h-3.5 text-[#5C2D91]" />
+                        {article.readTime}
+                      </span>
                     </div>
                 </div>
               </Card>
