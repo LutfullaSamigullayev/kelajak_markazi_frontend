412022797bd65c331bf3aaeb5ed860f670511811
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-09-06 20:26:00 +0500

    created LessonSchedulePage

---
+++ app/lesson-schedule/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { Sparkles, Calendar, Clock, MapPin, User, BookOpen } from "lucide-react";
+
+
+export default function LessonSchedulePage() {
+
+  return (
+    <div className="py-16 bg-[#F9F6FC] dark:bg-[#120924] min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-10">
+      </div>
+    </div>
+  );
+}
