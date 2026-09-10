b48dc46aa071ce67a3179b394188abc8409c3057
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-09-10 21:38:00 +0500

    created BamsPage

---
+++ app/bams/page.tsx
@@ -0,0 +1,13 @@
+import { regionsData } from "@/lib/data";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Sparkles, Building2, MapPin, Phone, Mail, Users, Award } from "lucide-react";
+
+export default function BamsPage() {
+  return (
+    <div className="py-16 bg-[#F9F6FC] dark:bg-[#120924] min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col gap-10">
+      </div>
+    </div>
+  );
+}
